76b13e483d4cab18caa869841b4e0688200b06c1
Janett Gerrard <mikanchik@gmail.com>
2023-10-08 14:04:22 +0100

    display dishes in table

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,11 @@
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
   import recipes from "./data/recipes.json"
+  import { TabGroup, Tab, Table } from '@skeletonlabs/skeleton';
+  import { AppShell } from '@skeletonlabs/skeleton';
+    import Dish from './lib/Dish.svelte';
+
+  let tabSet: number = 0;
 </script>
 
 <main>
@@ -16,13 +21,47 @@
   </div> -->
   <h1>Pokemon Sleep dish strength calculator</h1>
 
-  <div>
-    {#each recipes.curries as curry}
-        <div>
-          <p>{curry.Dish}: {curry.Base}</p>
-        </div>
-    {/each}
-  </div>
+  <TabGroup>
+    <Tab bind:group={tabSet} name="Curries" value={0}>
+      <!-- <svelte:fragment slot="lead">(icon)</svelte:fragment> -->
+      <span>Curries</span>
+    </Tab>
+    <Tab bind:group={tabSet} name="Salads" value={1}>Salads</Tab>
+    <Tab bind:group={tabSet} name="Desserts" value={2}>Desserts</Tab>
+    <!-- Tab Panels --->
+    <svelte:fragment slot="panel">
+      <div class="table-container">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th class="table-cell-fit">Strength</th>
+            </tr>
+          </thead>
+        {#if tabSet === 0}
+          <tbody>
+            {#each recipes.curries as curry}
+                <Dish dish={curry} />
+            {/each}
+          </tbody>
+        {:else if tabSet === 1}
+          <tbody>
+            {#each recipes.salads as salad}
+              <Dish dish={salad} />
+            {/each}
+          </tbody>
+        {:else if tabSet === 2}
+          <tbody>
+            {#each recipes.desserts as dessert}
+              <Dish dish={dessert} />
+            {/each}
+          </tbody>
+        {/if}
+        </table>
+      </div>
+    </svelte:fragment>
+  </TabGroup>
+
 
 </main>
 
---
+++ src/lib/Dish.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let dish: any;
+</script>
+
+<tr>
+  <td class="text-left">{dish.Dish}</td>
+  <td class="table-cell-fit">{dish.Base}</td>
+</tr>
