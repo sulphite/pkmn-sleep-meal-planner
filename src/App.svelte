206b35f25bb9fb1d0610ad660a6f63b236115b0d
Janett Gerrard <mikanchik@gmail.com>
2023-10-08 11:06:23 +0100

    display recipes

--- src/App.svelte
+++ src/App.svelte
@@ -2,30 +2,28 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import recipes from "./data/recipes.json"
 </script>
 
 <main>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
+  </div> -->
+  <h1>Pokemon Sleep dish strength calculator</h1>
+
+  <div>
+    {#each recipes.curries as curry}
+        <div>
+          <p>{curry.Dish}: {curry.Base}</p>
+        </div>
+    {/each}
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
