3b8231dea2e45635b4ecc6490a88d9e816449ada
Janett Gerrard <mikanchik@gmail.com>
2023-10-09 23:40:41 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pkmn-sleep-meal-planner/",
   plugins: [svelte()],
 })
