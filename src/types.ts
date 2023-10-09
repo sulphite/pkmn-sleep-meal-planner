0fc83bd0517077b7c026fb2586c9fa1539d52528
Janett Gerrard <mikanchik@gmail.com>
2023-10-09 19:19:40 +0100

    add type definitions

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export type Recipe = {
+  "Dish": string;
+  "Image": string;
+  "Base": number;
+  "Large Leek": number;
+  "Tasty Mushroom": number;
+  "Fancy Egg": number;
+  "Soft Potato": number;
+  "Fancy Apple": number;
+  "Fiery Herb": number;
+  "Bean Sausage": number;
+  "Moomoo Milk": number;
+  "Honey": number;
+  "Pure Oil": number;
+  "Warming Ginger": number;
+  "Snoozy Tomato": number;
+  "Soothing Cacao": number;
+  "Slowpoke Tail": number;
+  "Greengrass Soybeans": number;
+  "total base": number;
+  [key: string]: any;
+}
+
+export type Ingredient = {
+  "Name": string;
+  "Strength": number;
+  [key: string]: any;
+}
